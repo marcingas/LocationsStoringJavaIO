e9976445b5271630ac9d3706cba6fdb70404e935
Marcin <gasiorek.mail@gmail.com>
2023-02-28 11:34:01 +0100

    first commit new class locations to store locations

---
+++ src/com/MarcinDev/Locations.java
@@ -0,0 +1,10 @@
+package com.MarcinDev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Locations implements Map<Integer, Location> {
+    //create locations object to store our data
+    private static Map<Integer,Location>locations = new HashMap<Integer,Location>();
+
+}
