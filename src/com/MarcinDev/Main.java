06b2ff5fdbbd380e993e0f3ffa520d2696772da9
Marcin <gasiorek.mail@gmail.com>
2023-02-28 11:51:50 +0100

    added locations class with locations transfered from main class. (Working ok)

--- src/com/MarcinDev/Main.java
+++ src/com/MarcinDev/Main.java
@@ -7,38 +7,9 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static Locations locations = new Locations();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        Map<String, Integer>tempExit = new HashMap<>();
-        locations.put(0, new Location(0,"You are sitting in front of your computer learning", tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("W",2);
-        tempExit.put("E",3);
-        tempExit.put("S",4);
-        tempExit.put("N",5);
-        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building", tempExit));
-
-        tempExit= new HashMap<>();
-        tempExit.put("N",5);
-        locations.put(2, new Location(2,"You are at the top of a hill", tempExit));
-
-
-        tempExit= new HashMap<>();
-        tempExit.put("W",1);
-        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring", tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("N",1);
-        tempExit.put("W",2);
-        locations.put(4, new Location(4,"You are ina valley beside a stream", tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("S",1);
-        tempExit.put("W",2);
-        locations.put(5, new Location(5,"You are in the forest", tempExit));
 
         Map<String,String>vocabulary = new HashMap<>();
         vocabulary.put("NORTH", "N");
@@ -47,18 +18,13 @@ public class Main {
         vocabulary.put("EAST", "E");
         vocabulary.put("QUIT", "Q");
 
-
-
-
-
-
-
         int loc =  1;
         while(true){
             System.out.println(locations.get(loc).getDescription());
             if(loc==0){
                 break;
             }
+
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.println("Available exits are ");
             for(String exit: exits.keySet()) {
@@ -73,7 +39,6 @@ public class Main {
                         direction= vocabulary.get(word);
                         break;
                     }
-
                 }
             }
             if(exits.containsKey(direction)){
@@ -81,11 +46,6 @@ public class Main {
             } else{
                 System.out.println("Cops are there can not go there! ");
             }
-
-
-
         }
-
-
     }
 }
