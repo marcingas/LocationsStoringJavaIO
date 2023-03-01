125b8f233ffe4b8452b9a817f5b851d13f38363c
Marcin <gasiorek.mail@gmail.com>
2023-03-01 13:52:02 +0100

    code added for initiling the game's locations from a file locations.txt

--- src/com/MarcinDev/Locations.java
+++ src/com/MarcinDev/Locations.java
@@ -1,11 +1,9 @@
 package com.MarcinDev;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
     //create locations object to store our data
@@ -24,32 +22,56 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static{
-        Map<String, Integer>tempExit = new HashMap<>();
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(new FileReader("locations.txt"));
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()){
+                int loc = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String description = scanner.nextLine();
+                System.out.println("Imported loc: " + loc + ":" + description);
+                Map<String, Integer> tempExit = new HashMap<>();
+                locations.put(loc, new Location(loc,description,tempExit));
+            }
 
-        locations.put(0, new Location(0,"You are sitting in front of your computer learning", tempExit));
-        tempExit = new HashMap<>();
-        tempExit.put("W",2);
-        tempExit.put("E",3);
-        tempExit.put("S",4);
-        tempExit.put("N",5);
-        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building", tempExit));
-        tempExit= new HashMap<>();
-        tempExit.put("N",5);
-        locations.put(2, new Location(2,"You are at the top of a hill", tempExit));
-        tempExit= new HashMap<>();
-        tempExit.put("W",1);
-        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring", tempExit));
-        tempExit = new HashMap<>();
-        tempExit.put("N",1);
-        tempExit.put("W",2);
-        locations.put(4, new Location(4,"You are ina valley beside a stream", tempExit));
-        tempExit = new HashMap<>();
-        tempExit.put("S",1);
-        tempExit.put("W",2);
-        locations.put(5, new Location(5,"You are in the forest", tempExit));
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(scanner != null){
+                scanner.close();
+            }
+        }
+//        Map<String, Integer>tempExit = new HashMap<>();
+//
+//        locations.put(0, new Location(0,"You are sitting in front of your computer learning", tempExit));
+//        tempExit = new HashMap<>();
+//        tempExit.put("W",2);
+//        tempExit.put("E",3);
+//        tempExit.put("S",4);
+//        tempExit.put("N",5);
+//        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building", tempExit));
+//        tempExit= new HashMap<>();
+//        tempExit.put("N",5);
+//        locations.put(2, new Location(2,"You are at the top of a hill", tempExit));
+//        tempExit= new HashMap<>();
+//        tempExit.put("W",1);
+//        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring", tempExit));
+//        tempExit = new HashMap<>();
+//        tempExit.put("N",1);
+//        tempExit.put("W",2);
+//        locations.put(4, new Location(4,"You are ina valley beside a stream", tempExit));
+//        tempExit = new HashMap<>();
+//        tempExit.put("S",1);
+//        tempExit.put("W",2);
+//        locations.put(5, new Location(5,"You are in the forest", tempExit));
+//    }
+//    @Override
+//    public int size() {
+//        return locations.size();
     }
     @Override
-    public int size() {
+   public int size() {
         return locations.size();
     }
 
