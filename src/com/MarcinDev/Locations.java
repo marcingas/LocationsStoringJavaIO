98f9a69d6d109397f40d3471eddf1455f0dcf6ca
Marcin <gasiorek.mail@gmail.com>
2023-03-02 12:14:32 +0100

    modified Scaner in locations data for BufferedRreader

--- src/com/MarcinDev/Locations.java
+++ src/com/MarcinDev/Locations.java
@@ -24,7 +24,7 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static{
-        try(Scanner scanner = new Scanner(new FileReader("locations_big.txt"))){
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))){
             scanner.useDelimiter(",");
             while (scanner.hasNextLine()){
                 int loc = scanner.nextInt();
@@ -43,12 +43,6 @@ public class Locations implements Map<Integer, Location> {
         try(BufferedReader dirFile = new BufferedReader(new BufferedReader(new FileReader("directions_big.txt")));){
             String input;
             while((input = dirFile.readLine()) != null){
-//                int loc = scanner.nextInt();
-//                scanner.skip(scanner.delimiter());
-//                String direction = scanner.next();
-//                scanner.skip(scanner.delimiter());
-//                String dest = scanner.nextLine();
-//                int destination = Integer.parseInt(dest);
                 String [] data = input.split(",");
                 int loc = Integer.parseInt(data[0]);
                 String direction = data[1];
@@ -60,30 +54,6 @@ public class Locations implements Map<Integer, Location> {
         }catch (IOException e){
             e.printStackTrace();
         }
-
-//        Map<String, Integer>tempExit = new HashMap<>();
-//
-//        locations.put(0, new Location(0,"You are sitting in front of your computer learning", tempExit));
-//        tempExit = new HashMap<>();
-//        tempExit.put("W",2);
-//        tempExit.put("E",3);
-//        tempExit.put("S",4);
-//        tempExit.put("N",5);
-//        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building", tempExit));
-//        tempExit= new HashMap<>();
-//        tempExit.put("N",5);
-//        locations.put(2, new Location(2,"You are at the top of a hill", tempExit));
-//        tempExit= new HashMap<>();
-//        tempExit.put("W",1);
-//        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring", tempExit));
-//        tempExit = new HashMap<>();
-//        tempExit.put("N",1);
-//        tempExit.put("W",2);
-//        locations.put(4, new Location(4,"You are ina valley beside a stream", tempExit));
-//        tempExit = new HashMap<>();
-//        tempExit.put("S",1);
-//        tempExit.put("W",2);
-//        locations.put(5, new Location(5,"You are in the forest", tempExit));
     }
 
     @Override
