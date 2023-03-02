ba46c86048f27f56552180a7485440a629c520ea
Marcin <gasiorek.mail@gmail.com>
2023-03-02 11:56:45 +0100

    added -> change try finally for -> try with resources remove scanner (we don't nedd it anymore)

--- src/com/MarcinDev/Locations.java
+++ src/com/MarcinDev/Locations.java
@@ -24,9 +24,7 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static{
-        Scanner scanner = null;
-        try{
-            scanner = new Scanner(new FileReader("locations_big.txt"));
+        try(Scanner scanner = new Scanner(new FileReader("locations_big.txt"))){
             scanner.useDelimiter(",");
             while (scanner.hasNextLine()){
                 int loc = scanner.nextInt();
@@ -39,23 +37,18 @@ public class Locations implements Map<Integer, Location> {
 
         }catch (IOException e){
             e.printStackTrace();
-        }finally {
-            if(scanner != null){
-                scanner.close();
-            }
         }
+
         //read the exits:
-        try{
-            scanner = new Scanner(new BufferedReader(new FileReader("directions_big.txt")));
-            scanner.useDelimiter(",");
-            while(scanner.hasNextLine()){
+        try(BufferedReader dirFile = new BufferedReader(new BufferedReader(new FileReader("directions_big.txt")));){
+            String input;
+            while((input = dirFile.readLine()) != null){
 //                int loc = scanner.nextInt();
 //                scanner.skip(scanner.delimiter());
 //                String direction = scanner.next();
 //                scanner.skip(scanner.delimiter());
 //                String dest = scanner.nextLine();
 //                int destination = Integer.parseInt(dest);
-                String input = scanner.nextLine();
                 String [] data = input.split(",");
                 int loc = Integer.parseInt(data[0]);
                 String direction = data[1];
@@ -66,11 +59,8 @@ public class Locations implements Map<Integer, Location> {
             }
         }catch (IOException e){
             e.printStackTrace();
-        }finally {
-            if(scanner != null){
-                scanner.close();
-            }
         }
+
 //        Map<String, Integer>tempExit = new HashMap<>();
 //
 //        locations.put(0, new Location(0,"You are sitting in front of your computer learning", tempExit));
